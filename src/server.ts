b68c799b08f2a12e4452365c3d1c41b3756ebbc7
Albertino Augusto <albertinoaugusto@anima.co.mz>
2026-08-21 18:31:23 +0200

    start file changed

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 
 try {
-  await app.listen({ host: env.HOST, port: env.PORT });
-  //app.listen({ port: 3000, host: "0.0.0.0" });
+  //await app.listen({ host: env.HOST, port: env.PORT });
+  app.listen({ port: 3000, host: "0.0.0.0" });
 } catch (error) {
   app.log.error(error);
-  process.exit(1);
+  //process.exit(1);
 }
 
 
